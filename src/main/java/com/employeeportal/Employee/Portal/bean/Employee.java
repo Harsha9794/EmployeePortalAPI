0da06784ce5d1d13955a3a93599ebeb8ea60bf7b
Sai Harsha <sai.harsha94@gmail.com>
2020-11-17 13:17:34 +0530

    final commit

--- src/main/java/com/employeeportal/Employee/Portal/bean/Employee.java
+++ src/main/java/com/employeeportal/Employee/Portal/bean/Employee.java
@@ -6,11 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.sun.istack.NotNull;
+
 
 @Entity
 @Component
